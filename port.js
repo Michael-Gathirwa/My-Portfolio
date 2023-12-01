7a78b9d490b9e41329bbb7294ae610fad2260cde
Michael-Gathirwa <mikekenn8791@gmail.com>
2023-12-01 12:38:13 +0300

    Changes

--- port.js
+++ port.js
@@ -1,3 +1,19 @@
+const wordsArray=['Frontend Developer','UX/IX Designer', 'Web developer'];
+
+function changeText(){
+    const changeTextElement = document.getElementById('text')
+    let currentIndex=0;
+
+    setInterval(() =>{
+        changingTextElement.textContent = wordsArray[currentIndex];
+        currentIndex = (currentIndex + 1) % wordsArray.length;   
+    }, 1000);
+}
+changeText();
+
+
+
+
 var typed= new typed(".text", {
     Strings:["Frontend Developer","UX/IX Designer", "Web developer"],
     typeSpeed:100,
